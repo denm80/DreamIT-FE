578cc0c35315d3a0ddf317f0f1a4e31c9205969a
Denis Mikhailov <mikhailov.denis@tut.by>
2016-07-05 16:35:41 +0300

    layout

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,20 @@ var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 var browserify = require("gulp-browserify");
+var plumber = require('gulp-plumber');
 
 
 var path = {
     src: {
         html: './src/**/*.html',
+        images: './src/img/**',
         scripts: './src/**/*.js',
         styles: './src/**/*.scss'
     },
-    build: './build',
+    output: {
+        basedir: './build',
+        images: './build/img/',
+    },
     lib: './bower_components'
 };
 
@@ -22,13 +27,13 @@ var path = {
 // Clean
 // ------------------------------------------------------------------------------
 gulp.task('clean', function () {
-    del.sync(path.build);
+    del.sync(path.output.basedir);
 });
 
 // ------------------------------------------------------------------------------
 // Build
 // ------------------------------------------------------------------------------
-gulp.task('build', ['clean', 'scripts', 'styles', 'html'], function () {
+gulp.task('build', ['clean', 'scripts', 'styles', 'html', 'images'], function () {
 });
 
 
@@ -39,6 +44,7 @@ gulp.task('watch', function () {
     gulp.watch(path.src.scripts, ['scripts'], browserSync.reload);
     gulp.watch(path.src.styles, ['styles'], browserSync.reload);
     gulp.watch(path.src.html, ['html'], browserSync.reload);
+    gulp.watch(path.src.images, ['images'], browserSync.reload);
 });
 
 
@@ -49,7 +55,7 @@ gulp.task('serve', ['build', 'watch'], function () {
 
     browserSync.init({
         server: {
-            baseDir: path.build,
+            baseDir: path.output.basedir,
             routes: {
                 '/lib': path.lib
             }
@@ -59,11 +65,30 @@ gulp.task('serve', ['build', 'watch'], function () {
 
 
 // ------------------------------------------------------------------------------
+// Error Handler
+// ------------------------------------------------------------------------------
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
+
+// ------------------------------------------------------------------------------
 // Html
 // ------------------------------------------------------------------------------
 gulp.task('html', function () {
     gulp.src(path.src.html)
-        .pipe(gulp.dest(path.build))
+        .pipe(plumber({ errorHandler: handleError }))
+        .pipe(gulp.dest(path.output.basedir))
+        .pipe(browserSync.stream());
+});
+
+// ------------------------------------------------------------------------------
+// Images
+// ------------------------------------------------------------------------------
+gulp.task('images', function () {
+    gulp.src(path.src.images)
+        .pipe(plumber({ errorHandler: handleError }))
+        .pipe(gulp.dest(path.output.images))
         .pipe(browserSync.stream());
 });
 
@@ -73,6 +98,7 @@ gulp.task('html', function () {
 // ------------------------------------------------------------------------------
 gulp.task('scripts', function () {
     return gulp.src(path.src.scripts)
+        .pipe(plumber({ errorHandler: handleError }))
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(browserify({
@@ -80,7 +106,7 @@ gulp.task('scripts', function () {
         }))
         .pipe(concat('app.js'))
         .pipe(sourcemaps.write("/map"))
-        .pipe(gulp.dest(path.build))
+        .pipe(gulp.dest(path.output.basedir))
         .pipe(browserSync.stream());
 });
 
@@ -90,11 +116,12 @@ gulp.task('scripts', function () {
 // ------------------------------------------------------------------------------
 gulp.task('styles', function () {
     return gulp.src(path.src.styles)
+        .pipe(plumber({ errorHandler: handleError }))
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(concat('app.css'))
         .pipe(sourcemaps.write('/map'))
-        .pipe(gulp.dest(path.build))
+        .pipe(gulp.dest(path.output.basedir))
         .pipe(browserSync.stream());
 });
 
